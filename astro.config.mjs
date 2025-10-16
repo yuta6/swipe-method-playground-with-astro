ff57db07589cbd002d5df8976ecca506dbe07e16
yuta6 <46110512+yuta6@users.noreply.github.com>
2025-10-17 00:16:44 +0900

    add alias and rename the components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import path from "path";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("src"),
+      },
+    },
+  },
+});
